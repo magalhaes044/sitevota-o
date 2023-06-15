9b094a58f016f8615aa82fc2ffe2b8a0672f041d
magalhaes044 <129965153+magalhaes044@users.noreply.github.com>
2023-06-15 02:07:48 -0300

    vote4.0

--- script.js
+++ script.js
@@ -1,59 +1,72 @@
-const players = ['neymar', 'messi', 'mbappe', 'vinicius', 'haaland'];
-
-let player1 = null;
-let player2 = null;
-
-let player1Score = 0;
-let player2Score = 0;
-
-function generateNextDuel() {
-  if (players.length < 2) {
-    alert('A votação foi concluída!');
-    return;
+const players = [
+    { name: 'Neymar', score: 0, image: 'https://hdqwalls.com/download/neymar-jr-brazil-portraits-f1-3840x2400.jpg' },
+    { name: 'Messi', score: 0, image: 'https://th.bing.com/th/id/R.8bdfc9a6588b52996117a2d2a3219e95?rik=BVLPQYw%2febS%2b4g&pid=ImgRaw&r=0' },
+    { name: 'Mbappe', score: 0, image: 'https://th.bing.com/th/id/R.ea2b1b5815ae3aadfb9f5956afc28023?rik=j3wjEIX0dg2AnA&pid=ImgRaw&r=0' },
+    { name: 'Vinicius Junior', score: 0, image: 'https://wallpapercave.com/wp/wp11318284.jpg' },
+    { name: 'Haaland', score: 0, image: 'https://images.wallpapersden.com/image/download/hd-erling-haaland-2022_bWppbmyUmZqaraWkpJRnamtlrWZpaWU.jpg' }
+  ];
+  
+  let currentDuel = null;
+  
+  function vote(playerIndex) {
+    if (currentDuel !== null) {
+      const duelResultText = document.querySelector('.duel-result-text');
+      const winner = players[playerIndex];
+      const loser = players.find((player, index) => index !== playerIndex);
+      
+      if (currentDuel === winner) {
+        duelResultText.textContent = `Você votou corretamente! ${winner.name} é o mais bonito!`;
+        winner.score++;
+      } else {
+        duelResultText.textContent = `Você votou no jogador errado. ${winner.name} não é o mais bonito!`;
+        loser.score++;
+      }
+  
+      currentDuel = null;
+      updateScoreboard();
+    }
   }
-
-  const randomIndex1 = Math.floor(Math.random() * players.length);
-  player1 = players[randomIndex1];
-  players.splice(randomIndex1, 1);
-
-  const randomIndex2 = Math.floor(Math.random() * players.length);
-  player2 = players[randomIndex2];
-  players.splice(randomIndex2, 1);
-
-  const duelImages = document.querySelector('.duel-images');
-  duelImages.innerHTML = `
-    <img src="link_web_${player1}.jpg" alt="${player1}" class="duel-image" onclick="vote('${player1}')">
-    <img src="link_web_${player2}.jpg" alt="${player2}" class="duel-image" onclick="vote('${player2}')">
-  `;
-}
-
-function vote(player) {
-  if (player === player1) {
-    player1Score++;
-  } else if (player === player2) {
-    player2Score++;
+  
+  function updateScoreboard() {
+    for (let i = 0; i < players.length; i++) {
+      const player = players[i];
+      const scoreElement = document.getElementById(`score-${player.name.toLowerCase().replace(' ', '-')}`);
+      scoreElement.textContent = player.score;
+    }
   }
-
-  updateScores();
-  generateNextDuel();
-}
-
-function updateScores() {
-  const neymarCount = document.getElementById('neymar-count');
-  const messiCount = document.getElementById('messi-count');
-  const mbappeCount = document.getElementById('mbappe-count');
-  const viniciusCount = document.getElementById('vinicius-count');
-  const haalandCount = document.getElementById('haaland-count');
-
-  neymarCount.textContent = player1Score;
-  messiCount.textContent = player2Score;
-
-  // Atualize as contagens para os outros jogadores aqui
-
-  // Exemplo:
-  // mbappeCount.textContent = player3Score;
-  // viniciusCount.textContent = player4Score;
-  // haalandCount.textContent = player5Score;
-}
-
-generateNextDuel();
+  
+  function nextDuel() {
+    if (players.length < 2) {
+      const duelResultText = document.querySelector('.duel-result-text');
+      duelResultText.textContent = 'A votação foi concluída!';
+      return;
+    }
+  
+    const randomIndex1 = Math.floor(Math.random() * players.length);
+    const player1 = players[randomIndex1];
+    players.splice(randomIndex1, 1);
+  
+    const randomIndex2 = Math.floor(Math.random() * players.length);
+    const player2 = players[randomIndex2];
+    players.splice(randomIndex2, 1);
+  
+    currentDuel = player1;
+  
+    const duelResultText = document.querySelector('.duel-result-text');
+    duelResultText.textContent = '';
+  
+    const duelImages = document.querySelector('.duel-images');
+    duelImages.innerHTML = `
+      <div class="player" onclick="vote(0)">
+        <img src="${player1.image}" alt="${player1.name}">
+        <span class="player-name">${player1.name}</span>
+      </div>
+      <div class="player" onclick="vote(1)">
+        <img src="${player2.image}" alt="${player2.name}">
+        <span class="player-name">${player2.name}</span>
+      </div>
+    `;
+  }
+  
+  nextDuel();
+  
