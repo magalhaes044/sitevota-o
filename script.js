0284b728385542900620d817114dbca7843e3d43
magalhaes044 <129965153+magalhaes044@users.noreply.github.com>
2023-06-15 01:37:39 -0300

    vote2.0

--- script.js
+++ script.js
@@ -1,36 +1,41 @@
-let neymarCount = 0;
-let messiCount = 0;
-let mbappeCount = 0;
-let haalandCount = 0;
+const players = ['neymar', 'messi', 'mbappe', 'vinicius', 'haaland'];
 
-function vote(player1, player2) {
-  const random = Math.random();
-  if (random < 0.5) {
-    updateScore(player1);
-  } else {
-    updateScore(player2);
+let player1 = null;
+let player2 = null;
+
+let player1Score = 0;
+let player2Score = 0;
+
+function generateDuel() {
+  if (players.length < 2) {
+    alert('A votação foi concluída!');
+    return;
+  }
+
+  const randomIndex1 = Math.floor(Math.random() * players.length);
+  player1 = players[randomIndex1];
+  players.splice(randomIndex1, 1);
+
+  const randomIndex2 = Math.floor(Math.random() * players.length);
+  player2 = players[randomIndex2];
+  players.splice(randomIndex2, 1);
+
+  const duelImages = document.querySelector('.duel-images');
+  duelImages.innerHTML = `
+    <img src="link_web_${player1}.jpg" alt="${player1}" class="duel-image">
+    <h3 class="versus">VS</h3>
+    <img src="link_web_${player2}.jpg" alt="${player2}" class="duel-image">
+  `;
+}
+
+function updateScore(winner) {
+  if (winner === player1) {
+    player1Score++;
+    document.getElementById(`${player1}-count`).innerText = player1Score;
+  } else if (winner === player2) {
+    player2Score++;
+    document.getElementById(`${player2}-count`).innerText = player2Score;
   }
 }
 
-function updateScore(player) {
-  switch (player) {
-    case 'neymar':
-      neymarCount++;
-      document.getElementById('neymar-count').innerText = neymarCount;
-      break;
-    case 'messi':
-      messiCount++;
-      document.getElementById('messi-count').innerText = messiCount;
-      break;
-    case 'mbappe':
-      mbappeCount++;
-      document.getElementById('mbappe-count').innerText = mbappeCount;
-      break;
-    case 'haaland':
-      haalandCount++;
-      document.getElementById('haaland-count').innerText = haalandCount;
-      break;
-    default:
-      break;
-  }
-}
+generateDuel();
